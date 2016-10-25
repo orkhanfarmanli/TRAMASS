be3ece9ff0b1d27303ab8a8d2f8850d75be10f15
sabuhitalibli <sabuhi.talibli@gmail.com>
2016-10-25 15:28:36 +0400

    style changes in about, login, register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
                     <form role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="control-label">E-Mail Address</label>
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
 
                                 @if ($errors->has('email'))
@@ -18,17 +18,10 @@
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
-                            <!--                            <label for="inputEmail1">Username or Email</label>-->
-<!--                            <input type="email" class="form-control" id="inputEmail1" placeholder="">-->
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-<!--
-                            <label for="inputPassword1">Password</label>
-                            <input type="password" class="form-control" id="inputPassword1" placeholder="">
-
--->
-                           <label for="password" class="col-md-4 control-label">Password</label>
+                           <label for="password" class="control-label">Password</label>
                            <input id="password" type="password" class="form-control" name="password">
                                @if ($errors->has('password'))
                                     <span class="help-block">
@@ -40,9 +33,6 @@
 
                         <a href="{{ url('/password/reset') }}">Forgot Password?</a><br>
                         <input type="submit" value="SIGN IN" class="btn btn-success btn-lg text-center col-md-12 btn-2">
-
-<!--                        <p class="text-center">OR</p>-->
-
                         <button type="submit" class="btn btn-primary btn-lg text-center col-md-12 btn-2">
                         <a href="#"  style="color:white !important">
                             <i class="fa fa-facebook" aria-hidden="true"></i> LOGIN WITH FACEBOOK
